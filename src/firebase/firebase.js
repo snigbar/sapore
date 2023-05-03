351ce97b586c78cccc8952102354c19146c7ead0
snigbar <snigbar@outlook.com>
2023-05-04 00:13:06 +0600

    first commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCc_rNGCv3dGrK1fWJu8LB90yCQq9wAqHA",
+  authDomain: "sapore-8dd43.firebaseapp.com",
+  projectId: "sapore-8dd43",
+  storageBucket: "sapore-8dd43.appspot.com",
+  messagingSenderId: "347242102667",
+  appId: "1:347242102667:web:019b056f5b942aead4f98e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1 className='text-red-800'>FDSF FDSFDSFS</h1>
   </React.StrictMode>,
 )
