0b11080216df527d08bcbafa67e3d06a9e83249e
snigbar <snigbar@outlook.com>
2023-05-04 00:22:20 +0600

    ready to go

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import router from './Providers/routes'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1 className='text-red-800'>FDSF FDSFDSFS</h1>
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
